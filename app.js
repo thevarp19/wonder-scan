6bddbaa7ebc0a7b5d76ace14a26ea93e394ef934
Yerlan Jumabayev <thevarp18@gmail.com>
2024-06-16 17:45:27 +0500

    refactor

--- app.js
+++ app.js
@@ -8,23 +8,8 @@ function getQueryParameterByKey(key) {
 
 function setupLiveReader(resultElement) {
     localStorage.removeItem(localKey);
-
-    var container = document.createElement("div");
-
-    container.style.position = "absolute";
-    container.style.zIndex = "999";
-    container.style.width = "100%";
-    container.style.height = "100%";
-    container.style.left = "0";
-    container.style.top = "0";
-
-    var canvas = document.createElement("canvas");
-    var video = document.createElement("video");
-    var context = canvas.getContext("2d");
-
-    canvas.style.position = "absolute";
-
-    container.appendChild(canvas);
+    const container = createContainer();
+    const { video, canvas, context } = setupVideoAndCanvas(container);
 
     document.body.insertBefore(container, resultElement);
 
@@ -35,165 +20,202 @@ function setupLiveReader(resultElement) {
         },
     };
 
-    var result_value = document.getElementById("result_value");
+    const resultValue = document.getElementById("result_value");
 
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function (stream) {
-            video.width = 320;
+        .then((stream) =>
+            initializeStream(stream, video, canvas, context, resultValue)
+        )
+        .catch(console.log);
 
-            BarcodeScanner.init();
-            BarcodeScanner.streamCallback = function (result) {
-                result_value.innerHTML = result[0].Value;
-                // BarcodeScanner.StopStreamDecode();
-                // video.pause();
-                // stream.getTracks()[0].stop();
-            };
-
-            video.setAttribute("autoplay", "");
-            video.setAttribute("playsinline", "");
-            video.setAttribute("style", "width: 100%");
-            video.srcObject = stream;
-            container.appendChild(video);
-            video.onloadedmetadata = function (e) {
-                var canvasSetting = {
-                    x: 50,
-                    y: 20,
-                    width: 200,
-                    height: 30,
-                };
-                var rect = video.getBoundingClientRect();
-                canvas.style.height = rect.height + "px";
-                canvas.style.width = rect.width + "px";
-                canvas.style.top = rect.top + "px";
-                canvas.style.left = rect.left + "px";
-                const overlayColor = "rgba(0,0,0,0.9)";
-                context.fillStyle = overlayColor;
-                context.fillRect(0, 0, rect.width, rect.height);
-                context.clearRect(
-                    canvasSetting.x,
-                    canvasSetting.y,
-                    canvasSetting.width,
-                    canvasSetting.height
-                );
-                context.strokeStyle = "#ff671f";
-                context.strokeRect(
-                    canvasSetting.x,
-                    canvasSetting.y,
-                    canvasSetting.width,
-                    canvasSetting.height
-                );
-                video.play();
-                BarcodeScanner.DecodeStream(video);
-            };
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
     setupButtons();
+}
+
+function createContainer() {
+    const container = document.createElement("div");
+    container.style.position = "absolute";
+    container.style.zIndex = "999";
+    container.style.width = "100%";
+    container.style.height = "100%";
+    container.style.left = "0";
+    container.style.top = "0";
+    return container;
+}
+
+function setupVideoAndCanvas(container) {
+    const canvas = document.createElement("canvas");
+    const video = document.createElement("video");
+    const context = canvas.getContext("2d");
+
+    canvas.style.position = "absolute";
+    container.appendChild(canvas);
+
+    return { video, canvas, context };
+}
+
+function initializeStream(stream, video, canvas, context, resultValue) {
+    video.width = 320;
+
+    BarcodeScanner.init();
+    BarcodeScanner.streamCallback = (result) => {
+        resultValue.innerHTML = result[0].Value;
+    };
+
+    video.setAttribute("autoplay", "");
+    video.setAttribute("playsinline", "");
+    video.setAttribute("style", "width: 100%");
+    video.srcObject = stream;
+    video.onloadedmetadata = () => configureCanvas(video, canvas, context);
+    document.body.appendChild(video);
+}
+
+function configureCanvas(video, canvas, context) {
+    const canvasSetting = {
+        x: 50,
+        y: 20,
+        width: 200,
+        height: 30,
+    };
+    const rect = video.getBoundingClientRect();
+
+    canvas.style.height = `${rect.height}px`;
+    canvas.style.width = `${rect.width}px`;
+    canvas.style.top = `${rect.top}px`;
+    canvas.style.left = `${rect.left}px`;
+
+    const overlayColor = "rgba(0,0,0,0.9)";
+    context.fillStyle = overlayColor;
+    context.fillRect(0, 0, rect.width, rect.height);
+    context.clearRect(
+        canvasSetting.x,
+        canvasSetting.y,
+        canvasSetting.width,
+        canvasSetting.height
+    );
+    context.strokeStyle = "#ff671f";
+    context.strokeRect(
+        canvasSetting.x,
+        canvasSetting.y,
+        canvasSetting.width,
+        canvasSetting.height
+    );
+
+    video.play();
+    BarcodeScanner.DecodeStream(video);
 }
 
 function setupButtons() {
     const scanType = getQueryParameterByKey("type");
     if (scanType === "multiple") {
-        const confirmButton = document.getElementById("confirm_button");
-        confirmButton.style.display = "none";
+        hideElementById("confirm_button");
     } else {
-        const submitButton = document.getElementById("submit_button");
-        submitButton.style.display = "none";
-        const saveButton = document.getElementById("save_button");
-        saveButton.style.display = "none";
+        hideElementById("submit_button");
+        hideElementById("save_button");
     }
+
+    updateRedirectUrl();
+}
+
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) element.style.display = "none";
+}
+
+function updateRedirectUrl() {
     const mode = getQueryParameterByKey("mode");
     const useFor = getQueryParameterByKey("useFor");
-    if (mode === "dev") {
-        if (useFor) {
-            if (useFor === "employeeSearch") {
-                redirectUrl = "http://localhost:5173/employee/search";
-            } else if (useFor === "employeeSize") {
-                redirectUrl = "http://localhost:5173/employee/sizes";
-            } else {
-                redirectUrl = "http://localhost:5173/employee/scan";
-            }
-        } else redirectUrl = "http://localhost:5173/employee/scan";
+    const baseUrl =
+        mode === "dev"
+            ? "http://localhost:5173"
+            : "https://wonder-front.vercel.app";
+
+    if (useFor === "employeeSearch") {
+        redirectUrl = `${baseUrl}/employee/search`;
+    } else if (useFor === "employeeSize") {
+        redirectUrl = `${baseUrl}/employee/sizes`;
     } else {
-        if (useFor === "employeeSearch") {
-            redirectUrl = "https://wonder-front.vercel.app/employee/search";
-        } else if (useFor === "employeeSize") {
-            redirectUrl = "https://wonder-front.vercel.app/employee/sizes";
-        } else {
-            redirectUrl = "https://wonder-front.vercel.app/employee/scan";
-        }
+        redirectUrl = `${baseUrl}/employee/scan`;
     }
 }
 
 function confirmResult() {
-    const result_value = document.getElementById("result_value").innerText;
-    if (!result_value || result_value === "Value") {
-        return;
-    }
-    const isOk = confirm(`Подтвердить: ${result_value}`);
-    const step = getQueryParameterByKey("step") || 1;
+    const resultValue = document.getElementById("result_value").innerText;
+    if (!resultValue || resultValue === "Value") return;
+
+    const isOk = confirm(`Подтвердить: ${resultValue}`);
     if (isOk) {
-        window.location.href = `${redirectUrl}?type=single&result=${result_value}&step=${step}`;
+        const step = getQueryParameterByKey("step") || 1;
+        window.location.href = `${redirectUrl}?type=single&result=${resultValue}&step=${step}`;
     }
 }
 
 function saveResult() {
-    const result_value = document.getElementById("result_value");
-    let scannedResults = [];
-    try {
-        scannedResults = JSON.parse(localStorage.getItem(localKey)) || [];
-    } catch {}
+    const resultValueElement = document.getElementById("result_value");
+    let scannedResults = getLocalScannedResults();
 
     if (
-        !result_value.innerText ||
-        scannedResults.includes(result_value.innerText) ||
-        result_value.innerText === "Значение"
-    ) {
+        !resultValueElement.innerText ||
+        scannedResults.includes(resultValueElement.innerText) ||
+        resultValueElement.innerText === "Значение"
+    )
         return;
-    }
-    scannedResults.push(result_value.innerText);
-    try {
-        localStorage.setItem(localKey, JSON.stringify(scannedResults));
-        showNotification("Сохранено!");
-    } catch {}
+
+    scannedResults.push(resultValueElement.innerText);
+    saveToLocalStorage(localKey, scannedResults);
+    showNotification("Сохранено!");
 }
 
 function submitResult() {
-    let scannedResults = [];
+    let scannedResults = getLocalScannedResults();
+    if (
+        !confirm(
+            `Всего: ${scannedResults.length}\n${scannedResults.join("\n")}`
+        )
+    )
+        return;
+
+    const step = getQueryParameterByKey("step") || 1;
+    const baseLink = `${redirectUrl}?type=multiple&step=${step}&results=${encodeURIComponent(
+        scannedResults.join(",")
+    )}`;
+    window.location.href = baseLink;
+}
+
+function getLocalScannedResults() {
     try {
-        scannedResults = JSON.parse(localStorage.getItem(localKey)) || [];
-    } catch {}
-    let isOk = confirm(
-        `Всего: ${scannedResults.length}\n${scannedResults.join("\n")}`
-    );
-    if (isOk) {
-        const step = getQueryParameterByKey("step") || 1;
-        let baseLink = `${redirectUrl}?type=multiple&step=${step}`;
-        scannedResults.forEach((result) => {
-            baseLink += `&result=${result}`;
-        });
-        window.location.href = baseLink;
+        return JSON.parse(localStorage.getItem(localKey)) || [];
+    } catch {
+        return [];
     }
 }
 
-const NOTIFICATION_DURING = 1000;
+function saveToLocalStorage(key, value) {
+    try {
+        localStorage.setItem(key, JSON.stringify(value));
+    } catch (err) {
+        console.error("Failed to save to local storage", err);
+    }
+}
+
+const NOTIFICATION_DURATION = 1000;
 
 function showNotification(message) {
     const notification = document.getElementById("notification");
+    if (!notification) return;
+
     notification.innerText = message;
     notification.classList.remove("hidden");
     notification.style.opacity = 1;
 
-    setTimeout(function () {
+    setTimeout(() => {
         notification.style.opacity = 0;
         notification.addEventListener(
             "transitionend",
-            function () {
+            () => {
                 notification.classList.add("hidden");
             },
             { once: true }
         );
-    }, NOTIFICATION_DURING);
+    }, NOTIFICATION_DURATION);
 }
